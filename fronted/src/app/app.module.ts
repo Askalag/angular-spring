9dc2aab1cbcc8532e1c6140f350bb909675d8175
Askalag <askalag@gmail.com>
2018-12-30 18:39:33 +0300

    initial commit

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserComponent } from './shared/user/user.component';
+import { EndComponent } from './end/end.component';
+
+
+const appRoutes: Routes = [
+  { path: "usersList", component: UserListComponent},
+  { path: "", component: AppComponent},
+  { path: "users", component: UserListComponent},
+  { path: "users", component: UserListComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserListComponent,
-    UserComponent
+    UserComponent,
+    EndComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
